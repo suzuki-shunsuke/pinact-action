72f20977f73d3af8fb5c882499451d7847dff785
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2026-02-03 09:48:17 +0900

    chore: introduce eslint (#909)
    * chore: introduce eslint
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ eslint.config.ts
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.node,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+  tseslint.configs.recommended,
+  globalIgnores(["build/*", "dist/*"]),
+]);
